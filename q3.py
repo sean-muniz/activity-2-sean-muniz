9adcf46dc4b2b8f30b974fedc3de77a96fe2fd4c
sean-muniz <158625364+sean-muniz@users.noreply.github.com>
2024-02-13 13:00:57 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,17 @@
 def wordCount(t):
+    try: 
+        with open(t, 'r') as file:
+            names = file.readlines() 
+            for i in range(0, len(names)): 
+                names[i] = names[i].strip()
+            name_dictionary = {key: 0 for key in names} 
+            for key in name_dictionary: 
+                for name in names: 
+                    if key == name:
+                        name_dictionary[key] += 1
+        return name_dictionary
+    except FileNotFoundError: 
+        print("cannot find file")
+
+print(wordCount('dictionary.txt')); 
+

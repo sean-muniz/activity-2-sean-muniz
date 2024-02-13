e8ec6ad5a4e33ba57dca5639773b32d85dae5073
sean-muniz <158625364+sean-muniz@users.noreply.github.com>
2024-02-13 12:59:35 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,19 @@
-def getPrimeNumbers(n):
+def is_prime_num(num): 
+    if (num <= 1):
+        return False; 
+    else: 
+        for i in range(2, num): 
+            if (num % i == 0): 
+                return False; 
+            else: 
+                return True;
+
+def getPrimeNumbers(n): 
+    primes = []; 
+    for num in range(2, n+1):
+        if is_prime_num(num):
+            primes.append(num)
+    return primes;
+
+
+print(getPrimeNumbers(5))
